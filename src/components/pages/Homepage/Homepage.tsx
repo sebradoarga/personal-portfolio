8fda5a176a65ec0d5935a95fafdf150d89680222
raduOarga <raduoarga95@gmail.com>
2021-11-18 13:22:14 +0100

    style homepage buttons

--- src/components/pages/Homepage/Homepage.tsx
+++ src/components/pages/Homepage/Homepage.tsx
@@ -1,16 +1,25 @@
 import styled from "styled-components";
 import { device } from "../../../device";
-
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
 const Homepage = () => {
   return (
     <Wrapper>
       <Container>
-        <Header>Sebastian-Radu Oarga</Header>
-        <Buttons>
-          <Btn>About</Btn>
-          <Btn>Portfolio</Btn>
-          <Btn>Contact</Btn>
-        </Buttons>
+        <HorizontalAxis>
+          <Btn>
+            <ArrowBackIcon sx={{ fontSize: 40 }} />
+            About
+          </Btn>
+          <Header>Sebastian-Radu Oarga</Header>
+          <Btn>
+            Contact <ArrowForwardIcon sx={{ fontSize: 40 }} />
+          </Btn>
+        </HorizontalAxis>
+        <Btn id="portfolio-btn">
+          Portfolio <ArrowDownwardIcon sx={{ fontSize: 40 }} />
+        </Btn>
       </Container>
     </Wrapper>
   );
@@ -21,7 +30,6 @@ export default Homepage;
 const Wrapper = styled.div`
   height: 100vh;
   width: 100%;
-  background: black;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -29,56 +37,66 @@ const Wrapper = styled.div`
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  background-color: #f4d03f;
-  background-image: linear-gradient(132deg, #f4d03f 0%, #16a085 100%);
-
+  background-image: linear-gradient(
+    135.8deg,
+    rgba(26, 26, 29, 1) 27.1%,
+    rgba(111, 34, 50, 1) 77.5%
+  );
   background-size: 1000% 1000%;
   animation: animate 7.5s ease-in-out infinite;
 `;
 
 const Container = styled.div`
   background: none;
-  padding: 5rem 2rem;
-  border-radius: 10px;
   display: flex;
   flex-direction: column;
   align-items: center;
-  margin: 5rem;
+  width: 100%;
+  height: 100vh;
+  justify-content: center;
+  position: relative;
+`;
+
+const HorizontalAxis = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  justify-content: space-between;
 `;
 
 const Header = styled.h1`
   color: #fff;
   font-size: 5rem;
   text-align: center;
-`;
-
-const Buttons = styled.div`
-  display: flex;
-  margin-top: 5rem;
-  flex-direction: column;
-
-  @media ${device.laptop} {
-    flex-direction: row;
-  }
+  display: none;
 `;
 
 const Btn = styled.button`
-  background: black;
-  border: 0.2rem solid #ffffff73;
-  border-radius: 5px;
-  color: #ffffff73;
-  padding: 1rem 2rem;
+  background: none;
+  border: none;
+  color: white;
   text-transform: uppercase;
-  font-family: "Poppins";
-  font-size: 2rem;
+  font-size: 5rem;
+  font-weight: bold;
   margin-top: 2rem;
   cursor: pointer;
   transition: all 0.3s ease;
-  margin: 1rem;
+  margin: 2rem;
+  display: flex;
+  align-items: center;
+
+  & * {
+    margin: 0.5rem;
+  }
 
   &:hover {
-    transform: scale(1.05);
-    color: white;
-    border-color: white;
+    -webkit-mask-image: linear-gradient(
+      -75deg,
+      rgba(0, 0, 0, 0.6) 30%,
+      #000 50%,
+      rgba(0, 0, 0, 0.6) 70%
+    );
+    -webkit-mask-size: 200%;
+    animation: shine 1.2s infinite;
   }
 `;
