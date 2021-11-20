fce2a13473d6d9927f0ac2ea4e4efbfadb640912
raduOarga <raduoarga95@gmail.com>
2021-11-20 09:50:12 +0100

    style disappearing project info text in portfolio page

--- src/components/pages/Portfolio/Portfolio.tsx
+++ src/components/pages/Portfolio/Portfolio.tsx
@@ -25,7 +25,8 @@ const AboutPage = ({
       <Navbar />
       <PageContent>
         <CountryTrivia>
-          <Preview src={CountryTriviaPreview} alt="" />
+          <Preview src={CountryTriviaPreview} alt="" className="preview" />
+          <CTDescription className="project-description"></CTDescription>
         </CountryTrivia>
       </PageContent>
     </Wrapper>
@@ -37,24 +38,37 @@ export default AboutPage;
 const Wrapper = styled(motion.div)`
   min-height: 100vh;
   width: 100%;
-  background: transparent;
+  background: #f7f5f5;
 `;
 
-const PageContent = styled.div``;
+const PageContent = styled.div`
+  overflow-y: auto;
+  min-height: 100vh;
+`;
 
 const CountryTrivia = styled.div`
   margin-top: 5%;
-  margin-left: 25%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 15%;
 `;
 
 const Preview = styled.img`
-  border-radius: 10px;
+  border-radius: 50%;
   box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
   cursor: pointer;
-  transition: transform 0.3s ease;
-  animation: float 6s ease infinite;
+  transition: all 0.3s ease;
+  animation: float 15s ease infinite;
 
   &:hover {
-    transform: scale(1.05);
+    border-radius: 25px;
   }
+`;
+
+const CTDescription = styled.p`
+  font-size: 2rem;
+  max-width: 45rem;
+  line-height: 3rem;
+  letter-spacing: 0.2rem;
 `;
